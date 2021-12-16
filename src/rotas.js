1daea727ca8d409dda0619bd02e9c531a1ce9cff
Thiago Ferreira <tllucasferreira15@gmail.com>
2021-12-16 01:39:48 -0300

    Desafio Concluido

--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { login } = require('./controladores/login');
 const usuarios = require('./controladores/usuarios');
+const produtos = require('./controladores/produtos');
 
 const rotas = express();
 
@@ -8,10 +9,10 @@ rotas.post('/usuario', usuarios.cadastrarUsuario);
 rotas.post('/login', login);
 rotas.get('/usuario', usuarios.verPerfil);
 rotas.put('/usuario', usuarios.atualizarUsuario);
-rotas.get('/produtos', usuarios.listarProdutos);
-rotas.get('/produtos/:id', usuarios.detalharProduto);
-rotas.post('/produtos', usuarios.cadastrarProduto);
-rotas.put('/produtos/:id', usuarios.atualizarProduto);
-rotas.delete('/produtos/:id', usuarios.excluirProduto);
+rotas.get('/produtos', produtos.listarProdutos);
+rotas.get('/produtos/:id',produtos.detalharProduto);
+rotas.post('/produtos', produtos.cadastrarProduto);
+rotas.put('/produtos/:id', produtos.atualizarProduto);
+rotas.delete('/produtos/:id', produtos.excluirProduto);
 
 module.exports = rotas;
