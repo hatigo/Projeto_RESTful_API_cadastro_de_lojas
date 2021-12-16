6da705d77c11dd14960ecaee39401b42dffb3158
Thiago Ferreira <tllucasferreira15@gmail.com>
2021-12-16 15:17:41 -0300

    alteração no git ignore

--- src/conexao.js
+++ src/conexao.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'market_cubos',
-    password: 'OgAihtkil02mc@44',
+    password: '123456',
     port: 5432
 });
 
